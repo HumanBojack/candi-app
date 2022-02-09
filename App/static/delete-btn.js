fb8b74f392e3abe7a46e13e7ae98b9d2d7f3f20f
HumanBojack <spychalar59@gmail.com>
2022-02-09 14:20:51 +0100

    delete script in another file

---
+++ App/static/delete-btn.js
@@ -0,0 +1,9 @@
+document.querySelectorAll('.delete-btn').forEach(item => {
+  item.addEventListener("click", onDeleteClick)
+})
+function onDeleteClick(e){
+  e.preventDefault()
+    if(confirm('Etes-vous sur de vouloir supprimer la candidature?')){
+      location.replace(e.target.href)
+    }
+} 
