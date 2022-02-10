e044eea9ec86609c45375d2bebef8e5dfff734ac
HumanBojack <spychalar59@gmail.com>
2022-02-10 09:48:13 +0100

    config database url

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import os
 
 #Database initialization
-
-if os.environ.get('DATABASE_URL') is None:
+if os.environ.get("DATABASE_URL"):
+    path = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = path.replace("postgres://", "postgresql://", 1)
+else:
     basedir = os.path.abspath(os.path.dirname(__file__))
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
-else:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 
 SECRET_KEY = 'VerySecret'
 SECURITY_PASSWORD_SALT = 'MyVerySecretTwo'
