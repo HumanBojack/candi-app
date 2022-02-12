43fccbeae31668a86550885507263a468fb53ee1
HumanBojack <spychalar59@gmail.com>
2022-02-12 22:06:07 +0100

    Mails

--- App/jobs.py
+++ App/jobs.py
@@ -1,20 +1,53 @@
-from this import d
-from App import app
+from App import app, mail
 from .models import User, Candidacy
 from flask_apscheduler import APScheduler
+from flask import render_template
+from flask_mail import Message
 from datetime import datetime, timedelta
 
+
 def weekly_mail_to_users():
-  today = datetime.utcnow().date()
-  last_week = today - timedelta(days=7)
-  with scheduler.app.app_context():
-    # candidacies = Candidacy.query.filter(Candidacy.date >= last_week, Candidacy.date < today).all()
-    # candidacies = Candidacy.query.filter(datetime.strptime(Candidacy.date, "%Y-%m-%d") >= last_week & datetime.strptime(Candidacy.date, "%Y-%m-%d") < today)
-    # candidacies = Candidacy.query.filter(Candidacy.date >= last_week).all()
-    # print(candidacies)
-    # for candidacy in candidacies:
-    #   print("one candidacy")
-    print("done")
+    today = datetime.utcnow().date()
+    last_week = today - timedelta(days=7)
+    with scheduler.app.app_context():
+        candidacies = Candidacy.query.filter(
+            Candidacy.date >= last_week, Candidacy.date < today
+        ).all()
+
+        user_candidacies = dict()
+        for candidacy in candidacies:
+            # user = user_candidacies.get(candidacy.user_id, None)
+            # if user is None:
+            #     user_candidacies[candidacy.user_id] = list()
+            if user_candidacies.get(candidacy.user_id) is None:
+                user_candidacies[candidacy.user_id] = list()
+            user_candidacies[candidacy.user_id].append(candidacy)
+        print(user_candidacies)
+
+        for user_id, candidacies in user_candidacies.items():
+            # send a mail to every user with the candidacies that he need to contact once again
+            # print(User.query.get(user_id).email)
+            print(candidacies[0].company.name)
+            user = User.query.get(user_id)
+
+            msg = Message(
+                "Tu devrais relancer ces entreprises.",
+                sender="candi.app.mailer@gmail.com",
+                recipients=[user.email],
+            )  # mail could be an environnement variable
+
+            msg.html = render_template(
+                "/mail/weekly_renewal.html", user=user, candidacies=candidacies
+            )
+
+            try:
+                mail.send(msg)
+            except:
+                continue
+
+        # print(candidacy.contact_full_name)
+        print("done")
+
 
 scheduler = APScheduler()
 scheduler.init_app(app)
--- config.py
+++ config.py
@@ -1,15 +1,15 @@
 import os
 
-#Database initialization
+# Database initialization
 if os.environ.get("DATABASE_URL"):
-    path = os.environ.get('DATABASE_URL')
+    path = os.environ.get("DATABASE_URL")
     SQLALCHEMY_DATABASE_URI = path.replace("postgres://", "postgresql://", 1)
 else:
     basedir = os.path.abspath(os.path.dirname(__file__))
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "app.db")
 
-SECRET_KEY = 'VerySecret'
-SECURITY_PASSWORD_SALT = 'MyVerySecretTwo'
+SECRET_KEY = "VerySecret"
+SECURITY_PASSWORD_SALT = "MyVerySecretTwo"
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 # Jobs configuration
@@ -18,9 +18,7 @@ JOBS = [
         "id": "mailer",
         "func": "App.jobs:weekly_mail_to_users",
         "trigger": "interval",
-        "seconds": 10
+        "seconds": 20,
     }
 ]
 SCHEDULER_API_ENABLED = True
-
-
