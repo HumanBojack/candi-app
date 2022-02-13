fd4a88a537e257d0090357750e425a1242cdbc21
HumanBojack <spychalar59@gmail.com>
2022-02-13 15:59:30 +0100

    Cleaned js

--- App/static/delete-btn.js
+++ App/static/delete-btn.js
@@ -1,9 +1,9 @@
 document.querySelectorAll('.delete-btn').forEach(item => {
   item.addEventListener("click", onDeleteClick)
 })
-function onDeleteClick(e){
+function onDeleteClick(e) {
   e.preventDefault()
-    if(confirm('Etes-vous sur de vouloir supprimer la candidature?')){
-      location.replace(e.target.href)
-    }
+  if (confirm('Etes-vous sur de vouloir supprimer la candidature?')) {
+    location.replace(e.currentTarget.href)
+  }
 } 
